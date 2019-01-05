10fc49eb18a6d4230d742f63092f8f66ca182889
mack-wang <641212003@qq.com>
2019-01-05 15:44:36 +0800

    优化init，避免重复解析attributes

--- src/util/component.js
+++ src/util/component.js
@@ -1,5 +1,3 @@
-import parseAttributesToStates from '../common/tool'
-
 export default class YuComponent {
     states = {}
 
@@ -12,7 +10,7 @@ export default class YuComponent {
     initStates(statesValue) {
       // states优先于attributes
       // attributes只是为了方便组件初始化，改变attributes不能改变state
-      const attributeStates = parseAttributesToStates(this.node.attributes)
+      const attributeStates = this.parseAttributesToStates(this.node.attributes)
       const states = Object.assign(attributeStates, statesValue || {})
       this.setStates(states)
       this.node.setAttribute(':mounted', true)
@@ -29,5 +27,44 @@ export default class YuComponent {
       for (const key of Object.keys(states)) {
         this.setState(key, states[key])
       }
+    }
+
+    parseAttributesToStates(attributes) {
+      const states = {}
+
+      Array.from(attributes).forEach((item) => {
+        if (item.name.indexOf(':') === 0) {
+          // 空value为true '开头为字符串，1开头为数字, { [ 开头为json对象
+          const name = item.name.substr(1)
+          const start = item.value[0]
+          if (item.value === '') {
+            states[name] = true
+            return
+          }
+
+          if (start === '{' || start === '[') {
+            states[name] = JSON.parse(item.value)
+            return
+          }
+
+          if (/[0-9]/.test(start)) {
+            states[name] = Number(start)
+            return
+          }
+
+          if (item.value === 'true' || item.value === 'false') {
+            states[name] = item.value === 'true'
+            return
+          }
+
+          if (start === '$') {
+            states[name] = YU.Data[item.value.substr(1)]
+            return
+          }
+
+          states[name] = item.value
+        }
+      })
+      return states
     }
 }
--- src/util/init.js
+++ src/util/init.js
@@ -1,26 +1,39 @@
 import YU from '../index'
 import NODE_CLASSNAME from '../common/const'
-import parseAttributesToStates from '../common/tool'
+
+
+function getAttributeRefValue(nodeItem) {
+  const ref = nodeItem.getAttribute(':ref')
+  if (ref) {
+    if (ref[0] === '$') {
+      return YU.Data[ref.substr(1)]
+    }
+    return ref
+  }
+  return false
+}
 
 function getSingleComponent(componentType) {
   const component = {}
   const nodes = document.querySelectorAll(NODE_CLASSNAME[componentType])
   Array.from(nodes).forEach((item) => {
     // js优先
-    const states = parseAttributesToStates(item.attributes)
+    const mounted = item.getAttribute(':mounted') === 'true'
+    const ref = getAttributeRefValue(item)
     // 如果已经初始化过的，则不再重复初始化
-    if (!states.mounted) {
-      if (states.ref) {
-        component[states.ref] = new YU[componentType](item, states)
+    if (!mounted) {
+      if (ref) {
+        component[ref] = new YU[componentType](item)
       } else if (Array.isArray(component[componentType])) {
-        component[componentType].push(new YU[componentType](item, states))
+        component[componentType].push(new YU[componentType](item))
       } else {
-        component[componentType] = [new YU[componentType](item, states)]
+        component[componentType] = [new YU[componentType](item)]
       }
     }
   })
   return component
 }
+
 
 function getMultipleComponent(componentTypeList) {
   const components = {}
