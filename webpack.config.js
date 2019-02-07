b62e2e39b7d6564162d0f5d4cec6d078ef8e58b4
mack-wang <641212003@qq.com>
2019-02-07 19:12:53 +0800

    添加LICENSE

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const uglify = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -37,5 +38,28 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin('styles.css'),
+    // 压缩输出的 JS 代码
+    new uglify({
+      // 多嵌套了一层
+      uglifyOptions: {
+        compress: {
+          // 在UglifyJs删除没有用到的代码时不输出警告
+          warnings: false,
+          // 删除所有的 `console` 语句，可以兼容ie浏览器
+          drop_console: true,
+          // 内嵌定义了但是只用到一次的变量
+          collapse_vars: true,
+          // 提取出出现多次但是没有定义成变量去引用的静态值
+          reduce_vars: true,
+        },
+        output: {
+          // 最紧凑的输出
+          beautify: false,
+          // 删除所有的注释
+          comments: false,
+        },
+      },
+    }),
   ],
+
 }
