ff88c7d048b5f562f752745928a5a328ad3b0194
mack-wang <641212003@qq.com>
2019-02-17 22:36:53 +0800

    修改入口文件，并发布v1.1.1

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,55 +1,69 @@
 require('webpack')
 const path = require('path')
+const merge = require('webpack-merge')
 const Uglify = require('uglifyjs-webpack-plugin')
 
-module.exports = [
-  // 生成es5的/dist/index.js，专门给浏览器端使用
-  {
-    mode: 'production',
+const outputToWindow = {
+  output: {
+    filename: 'index.js',
+    library: 'yu',
+    libraryTarget: 'window',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
 
-    entry: './src/index.js',
+const outputToCommonjs2 = {
+  output: {
+    filename: 'index.js',
+    library: 'yu',
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'lib'),
+  },
+}
 
-    output: {
-      filename: 'index.js',
-      library: 'yu',
-      libraryTarget: 'window',
-      path: path.resolve(__dirname, 'dist'),
-    },
+const prodConfig = {
+  mode: 'production',
 
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: [
-                '@babel/plugin-transform-runtime',
-                '@babel/plugin-proposal-class-properties',
-              ],
-            },
+  entry: './src/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-proposal-class-properties',
+            ],
           },
         },
-      ],
-    },
-
-    plugins: [
-      new Uglify({
-        uglifyOptions: {
-          compress: {
-            warnings: false,
-            drop_console: true,
-            collapse_vars: true,
-            reduce_vars: true,
-          },
-          output: {
-            beautify: false,
-            comments: false,
-          },
-        },
-      }),
+      },
     ],
   },
+
+  plugins: [
+    new Uglify({
+      uglifyOptions: {
+        compress: {
+          warnings: false,
+          drop_console: true,
+          collapse_vars: true,
+          reduce_vars: true,
+        },
+        output: {
+          beautify: false,
+          comments: false,
+        },
+      },
+    }),
+  ],
+}
+
+module.exports = [
+  merge(prodConfig, outputToWindow),
+  merge(prodConfig, outputToCommonjs2),
 ]
